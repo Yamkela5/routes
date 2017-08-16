98886663d60a11358356ef3f178f9ace19d75dfe
Yamkela <owamjames1@gmail.com>
2017-08-16 17:02:01 +0200

    initial commit

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // parse application/x-www-form-urlencoded
+app.use(express.static('public'));
+
+// app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
-app.use(express.static('public'));
 //creating  get router that greets different users
  app.get('/greetings/:GreetedNames', function(req, res){
     greeted.push(req.params.GreetedNames);
@@ -32,13 +34,13 @@ app.get('/greeted', function(req, res){
 //  console.log("==================");
   console.log(greeted);
 
-  res.render('users/index', {users:greeted});
+  res.render('./index', {users:greeted});
 
 });
 
 //create an HTML form
 app.get('/users/greet', function(req, res){
-  res.render('users/greet');
+  res.render('./greet');
 
 //  console.log("==================");
   console.log(greeted);
@@ -49,7 +51,7 @@ app.get('/users/greet', function(req, res){
 app.post('/users/greet', function(req, res){
   var name=req.body.name
   users.push(name)
-  res.render('users/greet',{msg:'Hello ,' + name});
+  res.render('./greet',{msg:'Hello ,' + name});
 
 //  console.log("==================");
   console.log(name);
