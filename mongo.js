ce8b7a4366f2b09af61c7917014506a6a1e63a08
Yamkela <owamjames1@gmail.com>
2017-09-07 16:06:07 +0200

    adding mongoDB

--- mongo.js
+++ mongo.js
@@ -3,6 +3,7 @@ const MongoURL =process.env.MONGO_DB_URL || "mongodb://localhost/Greeteds";
 mongoose.connect(MongoURL, {
   useMongoClient: true
 });
+//yash
 exports.NameStorage = mongoose.model('NameStorage', {
   count: Number,
   name: String
